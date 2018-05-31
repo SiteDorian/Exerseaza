eb521bc1ee5b0fc317272b8d1ff44fbde99f0521
Dorian Gotonoaga <dj.dorian98@gmail.com>
2018-05-31 09:50:14 +0300

    Paypal

---
+++ application/controllers/paypal.php
@@ -0,0 +1,29 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed');
+
+Class Paypal extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function index()
+    {
+
+    }
+
+    public function thankyou()
+    {
+
+    }
+
+    public function cancelled()
+    {
+
+    }
+
+    public function process()
+    {
+
+    }
+}
